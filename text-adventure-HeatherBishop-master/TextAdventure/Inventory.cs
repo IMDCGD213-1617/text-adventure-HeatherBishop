e4c9d4630e471dcabad6d223dbcbe21585955188
HeatherBishop <s177801@ucs.ac.uk>
2016-10-21 12:02:15 +0100

    Add Item/Inventory Classes

---
+++ text-adventure-HeatherBishop-master/TextAdventure/Inventory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Inventory
+{
+    public int slotsX = 5, slotsY = 5; // 5x5 grid inventory
+    public List<Item> inventory = new List<Item>();
+
+    public
+
+
+   void Start()
+    {
+        //Set slots to Empty
+        for (int s = 0; s < slotsX * slotsY; s++)
+        {
+            inventory.Add(new Item());
+        }
+
+        //Get Scripts
+        dataBase = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<ItemDataBase>();
+
+        //Add Items For Testing
+        AddItem(0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            AddItem(0);
+
+        if (Input.GetKeyDown(KeyCode.R))
+            RemoveItem(0);
+    }
+
+    void AddItem(int id)//With stacking the currentStack count is being added to all of the items of the same id, and more items still fill the inv.
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].itemStack && inventory[i].itemID == id && inventory[i].itemCurrentStack < inventory[i].itemMaxStack)//Stacking
+            {
+                inventory[i].itemCurrentStack++;//This makes the actual item have a higher current stack
+                break;
+            }
+            else if (inventory[i].itemName == null)
+            {
+                for (int d = 0; d < dataBase.items.Count; d++)
+                {
+                    if (dataBase.items[d].itemID == id)
+                    {
+                        inventory[i] = dataBase.items[d];
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    void RemoveItem(int id)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].itemID == id && inventory[i].itemCurrentStack > 1)
+            {
+                inventory[i].itemCurrentStack--;
+                break;
+            }
+            else if (inventory[i].itemID == id)
+            {
+                inventory[i] = new Item();
+                break;
+            }
+        }
+    }
+}
---
+++ text-adventure-HeatherBishop-master/TextAdventure/Item.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{    
+	public class Item
+	{
+        //base class for all items.
+
+        //contains the name
+        public string itemName;
+        public int itemID;
+        //description
+        public string itemDescription;
+        //value
+        public int itemRarity;
+
+            public virtual void Upgrade()
+        {
+            Console.WriteLine("Upgrade Item.");
+        }
+
+        public Item(string name, int id,string description, int rarity)
+        {
+            itemName = name;
+            itemID = id;
+
+            itemDescription = description;
+            itemRarity = rarity;
+        }
+
+        public Item()
+        {
+            itemID = -1;
+        }
+
+	}
+
+
+
+    public class ItemDatabase
+    {
+        public List<Item> items = new List<Item>();
+
+        void Start()
+        {
+            items.Add(new Item("Test Item", 0, "You have found a sword", 3));
+        }
+    }
+}
